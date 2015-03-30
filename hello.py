1a88c9223286cfc30a6f1617e7b0d364baf6bb58
liruosong <lirs7635@qq.com>
2015-03-30 22:12:41 +0800

    hello

---
+++ hello.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Hello(object):
+	"""docstring for Hello"""
+	#def __init__(self, arg):
+	#	super(Hello, self).__init__()
+	#	self.arg = arg
+	
+	def hello(self, name='world'):
+		print ('Hello, %s' % name)
